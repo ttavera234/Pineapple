c15cda8a2e7b4f31e673cb86d59f7d87024d1f24
ttavera234 <ttavera234@gmail.com>
2025-08-23 22:33:42 -0400

    Setup PostgreSQL connection and app.route

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# PostgreSQL connection string:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:coqki2-civped-fenjAg@localhost:5432/Pineapple'
+
 db = SQLAlchemy(app)
 
+@app.route('/')
+def hello():
+    return "Hey!"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
