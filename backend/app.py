311b83c617ae6f3381b1cf8c3ff5f9eee99e1f0c
ttavera234 <ttavera234@gmail.com>
2025-08-23 23:47:48 -0400

    Implemented and tested 'GET' endpoint for both a single meal and all meals with postman.

--- backend/app.py
+++ backend/app.py
@@ -33,7 +33,8 @@ def format_meal(meal):
 def hello():
     return "Hey!"
 
-@app.route('/meal', methods = ['POST'])
+# Create a meal
+@app.route('/meals', methods = ['POST'])
 def create_meal():
     calories = request.json['calories']
     fat = request.json['fat']
@@ -44,6 +45,22 @@ def create_meal():
     db.session.commit()
     return format_meal(meal)
 
+# Get all meals
+@app.route('/meals', methods = ['GET'])
+def get_events():
+    meals = Meal.query.order_by(Meal.id.asc()).all()
+    meal_list = []
+    for meal in meals:
+        meal_list.append(format_meal(meal))
+
+    return {'meals': meal_list}
+
+# Get a single meal
+@app.route('/meals/<meal_id>', methods = ['GET'])
+def get_meal(meal_id):
+    meal = Meal.query.filter_by(id=meal_id).one() # Do not need `first` here since there will be no duplicate ids
+    formatted_meal = format_meal(meal)
+    return {'meal': formatted_meal}
 
 if __name__ == '__main__':
     with app.app_context():
